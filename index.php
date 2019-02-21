b177d360ec3e8315b69f383b84c371b521d13c63
Nathan Hunt <n@thanhunt.com>
2019-02-20 21:25:13 -0500

    initial commit of mostly empty files

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require('quotes.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Nathan Hunt</title>
+	<meta charset="utf-8">
+
+	<link href='/local.css' rel='stylesheet'>
+
+</head>
+<body>
+	<div id="roundrect-container">
+		<section id="intro">
+		  <h1>Musical scales</h1>
+		</section>
+
+
+		<section id="explanation">
+		</section>
+
+		<section id="options">
+		</section>
+</div>
+</body>
